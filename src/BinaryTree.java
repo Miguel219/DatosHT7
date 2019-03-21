58c32481bd79369cc738e6d01fffdc2ef8e1740c
sorozcov <42949349+sorozcov@users.noreply.github.com>
2019-03-20 20:12:44 -0600

    binaryTree

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,16 +1,19 @@
 import java.util.Iterator;
 
 public class BinaryTree<E>{
+	
 	protected E val; // value associated with node
 	protected BinaryTree<E> parent; // parent of node
 	protected BinaryTree<E> left, right; // children of node
+	
 	public BinaryTree()
 	// post: constructor that generates an empty node
 	{
 		val = null;
 		parent = null; 
 		left = right = this;
-	}	
+	}
+	
 	public BinaryTree(E value)
 	// post: returns a tree referencing value and two empty subtrees
 	{
@@ -31,37 +34,101 @@ public class BinaryTree<E>{
 			right = new BinaryTree<E>(); 
 		}
 		setRight(right);
-	}public BinaryTree<E> left()
+	}
+	public BinaryTree<E> left()
 	// post: returns reference to (possibly empty) left subtree
 	// post: returns reference to (possibly empty) left subtree
 	{
 		return left;
 	}
-	public BinaryTree<E> parent()
+	public BinaryTree<E> right()
+	// post: returns reference to (possibly empty) left subtree
+	// post: returns reference to (possibly empty) left subtree
+	{
+		return right;
+	}
+	public BinaryTree<E> parent(){
 	// post: returns reference to parent node, or null
+	return parent;
+	}
 	public void setLeft(BinaryTree<E> newLeft)
 	// post: sets left subtree to newLeft
 	// re-parents newLeft if not null
 	{
-		if (isEmpty()) 
+		if (isEmpty(newLeft)) {
 			return;
-		if (left != null && left.parent() == this) 
+		}
+		if (left != null && left.parent() == this) {
 			left.setParent(null);
+		}
 		left = newLeft;
 		left.setParent(this);
 		}
+	
+public void setRight(BinaryTree<E> newRight)
+// post: sets left subtree to newLeft
+// re-parents newLeft if not null
+{
+	if (isEmpty(newRight)) { 
+		return;
 	}
+	if (right != null && right.parent() == this) {
+		right.setParent(null);
+	}
+	right = newRight;
+	right.setParent(this);
+	}
+
 	protected void setParent(BinaryTree<E> newParent)
 	// post: re-parents this node to parent reference, or null
 	{
-		if (!isEmpty()) {
+		if (!isEmpty(newParent)) {
 			parent = newParent;
 		}
 	}
-	public Iterator<E> iterator()
+	
+	public void inOrder() {
+	    inOrder(parent);
+	  }
+	private void inOrder(BinaryTree<E> tree) { 
+		if (tree == null) 
+			{ return; } 
+		inOrder(tree.left);  
+		inOrder(tree.right);
+		}
+
+	//Referencia Read more: http://www.java67.com/2016/08/binary-tree-inorder-traversal-in-java.html#ixzz5ilhPBTlX
 	// post: returns an in-order iterator of the elements
-	public boolean isLeftChild()
+	
 	// post: returns true if this is a left child of parent
+	public boolean isLeftChild() {
+		if(parent==null) {
+			return false;
+		}else if(parent.left()==this){
+			 return true;
+		}else {
+			return false;
+		}
+	}
+	// post: returns true if this is a right child of parent
+		public boolean isRightChild() {
+			if(parent==null) {
+				return false;
+			}else if(parent.right()==this){
+				 return true;
+			}else {
+				return false;
+			}
+		}
+	//To check if it doesn't have child
+	 public boolean isLeaf() {
+	        return left == null && right == null;
+	    }
+	 
+	//To check if it is a root
+		 public boolean isRoot() {
+		        return parent == null;
+		    }
 	public E value()
 	// post: returns value associated with this node
 	{
@@ -71,5 +138,11 @@ public class BinaryTree<E>{
 	// post: sets the value associated with this node
 	{
 		val = value;
+	}
+	
+	public boolean isEmpty(BinaryTree<E> tree)
+	// post: sets the value associated with this node
+	{
+		return tree==null;
 	}
 }

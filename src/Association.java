3436a14302c03eb97d72670dd03e28aa471a59da
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2019-03-20 18:22:38 -0600

    Cambios

---
+++ src/Association.java
@@ -0,0 +1,13 @@
+
+public class Association<K, V> {
+	
+	private K english;
+	private V spanish;
+	
+	public Association(K english, V spanish) {
+		// TODO Auto-generated constructor stub
+		this.english = english;
+		this.spanish = spanish; 
+	}
+
+}
